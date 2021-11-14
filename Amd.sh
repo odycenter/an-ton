d626bcc63416ea8f0990f1a786e0f814edb58856
ody <ody@heyu.tw>
2021-11-14 14:23:46 +0800

    add multi GPU config

--- Amd.sh
+++ Amd.sh
@@ -17,8 +17,8 @@ tar xzf /opt/ton-miner/minertools-opencl-ubuntu-18.04-x86-64.tar.gz -C /opt/ton-
 cd /opt/ton-miner && curl -L -O https://newton-blockchain.github.io/global.config.json
 chmod 777 global.config.json
 # Write running parameter
-echo "/opt/ton-miner/tonlib-opencl-cli  -v 3  -C /opt/ton-miner/global.config.json  -e 'pminer start kf8kO6K6Qh6YM4ddjRYYlvVAK7IgyW8Zet-4ZvNrVsmQ4EOF EQACwiacIj-gfKXapcZmnOoX9G54t1rhAZUkq1L92VpmtVjO 0 32 0'  [-l logfile]" >> /etc/rc.local
+echo "/opt/ton-miner/tonlib-opencl-cli  -v 3  -C /opt/ton-miner/global.config.json  -e 'pminer start kf8kO6K6Qh6YM4ddjRYYlvVAK7IgyW8Zet-4ZvNrVsmQ4EOF EQACwiacIj-gfKXapcZmnOoX9G54t1rhAZUkq1L92VpmtVjO [0 1 2 3 4 5 6 7] 32 0'  [-l logfile]" >> /etc/rc.local
 
 echo "Ton Mining Tools finish ..."
 # Reboot
-shutdown –r Now
+shutdown -r now
