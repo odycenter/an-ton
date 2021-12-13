99262559731a58d95a26240df7fc0941983d77d3
ody <ody@weexglobal.com>
2021-12-13 09:50:35 +0800

    add diff folder

--- install.sh
+++ install.sh
@@ -5,18 +5,21 @@ echo "Ton Mining Tools processing ..."
 giver_address="Uf_hLYWdlWIvlceh2YMYku3P6OFGECQGiyz_US6c-Wc3hwkF"
 my_address="EQACwiacIj-gfKXapcZmnOoX9G54t1rhAZUkq1L92VpmtVjO"
 global_config="/opt/ton-miner/global.config.json"
+# (for Nvidia)
 ton_miner="/opt/ton-miner/tonlib-cuda-cli"
+# (for AMD)
+# ton_miner="/opt/ton-miner/tonlib-opencl-cli"
 miner=7
 # Update system path
 apt-get update && apt-get upgrade
-# Install necessary package (for AMD vga)
+# Install necessary package (for AMD)
 apt-get install sudo vim wget curl systemd git ntp opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev
 # Set mining folder
 if [[ ! -f "/opt/ton-miner/" ]]; then
     sudo mkdir -p /opt/ton-miner/
 fi
 cd /opt/ton-miner
-# Install Nvidia cuda (for Nvidia vga)
+# Install Nvidia cuda (for Nvidia)
 if [[ ! -e "/opt/ton-miner/cuda-ubuntu1804.pin" ]]; then
     apt-get install gnupg gnupg2 software-properties-common
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
